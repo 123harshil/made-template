79b1db2fbb433dc3b232363d76cb594ee45aaef0
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-19 16:11:05 +0530

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -3,7 +3,7 @@
 # Execute your data pipeline
 # Replace this with the command or script that runs your data pipeline
 # For example:
-python data_pipeline.py
+python trafficprediction.py
 
 # Check if the database files exist
 db_file_1="D:\project2\Traffic.db"
