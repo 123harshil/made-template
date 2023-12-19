cd8e1bf1cc687762450d9ee0678342c951b49557
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-19 19:21:21 +0530

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+import os
 
-# Execute your data pipeline
-python project/trafficprediction.py  # Update this line with the correct relative path
+def test_db_files_created():
 
-# Check if the database files exist
-db_file_1="D:/project2/Traffic.db"  # Update these paths with the correct database paths
-db_file_2="D:/project2/weather.db"
+    db_file_1 = r"D:\project2\Traffic.db"
+    db_file_2 = r"D:\project2\weather.db"
 
-if [ -f "$db_file_1" ] && [ -f "$db_file_2" ]; then
-  echo "Databases created successfully!"
-else
-  echo "Error: Databases not created."
-fi
+    if not os.path.exists(db_file_1):
+        print(f"Error: {db_file_1} does not exist.")
+    if not os.path.exists(db_file_2):
+        print(f"Error: {db_file_2} does not exist.")
+
+if __name__ == '__main__':
+    test_db_files_created()
