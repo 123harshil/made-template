2ad7f987e9e4035ed694c867492fb82fb1994a40
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-20 00:57:11 +0530

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,1 +1,13 @@
-python databasetest.py
+#!/bin/bash
+
+# Execute your data pipeline (pipeline.sh)
+/project/pipeline.sh
+
+# Validate the existence of output files
+if [ -f "/data/Traffic.db" ] && [ -f "/data/weather.db" ]; then
+    echo "Output files 'Traffic.db' and 'weather.db' exist."
+    exit 0  # Exit with code 0 to indicate success
+else
+    echo "Error: Output files not found."
+    exit 1  # Exit with non-zero code to indicate test failure
+fi
