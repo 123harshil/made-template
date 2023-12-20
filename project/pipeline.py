0f3e27670d28675e743a18861279bebcdd0c1c6d
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-21 01:25:06 +0530

    Update pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -16,7 +16,7 @@ od.download(dataset_links[0])
 # storing the elon must tweets into a sqlite database
 traffic_data = './traffic-prediction-dataset/Traffic.csv'
 df1 = pd.read_csv(traffic_data)
-traffic_db_path = '../made/traffic.sqlite'
+traffic_db_path = "D:\\made\\traffic.sqlite"
 logging.info(f'storing the traffic data into a database under data folder ')
 conn = sqlite3.connect(traffic_db_path)
 df1.to_sql('traffic', conn, index=False, if_exists='replace')
@@ -26,7 +26,7 @@ od.download(dataset_links[1])
 # storing the tesla tweets into a sqlite dataase
 weather_data = 'weather-dataset/weatherHistory.csv'
 df2 = pd.read_csv(weather_data)
-conn = sqlite3.connect("../made/weather.sqlite")
+conn = sqlite3.connect("D:\\made\\weather.sqlite")
 logging.info(f'storing the weather data into a database under data folder ')
 df2.to_sql('weather', conn, index=False, if_exists='replace')
 # closing the database connection
