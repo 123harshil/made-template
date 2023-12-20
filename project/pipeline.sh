a87b3dea48b9df0bac83bf4e43d2d3aeec145762
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-21 01:06:05 +0530

    Update pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,4 +1,4 @@
 pip install opendatasets # to download the datasets
 pip install kaggle # to authenticate
 
-python3 get_data.py # to run the python file
+python3 pipeline.py # to run the python file
