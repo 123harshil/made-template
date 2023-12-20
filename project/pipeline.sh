eda9c9868fa3d07bf5275d2bd4248f31a7ae2ca4
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-20 15:19:20 +0530

    Update pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -2,4 +2,4 @@ KAGGLE_JSON_PATH="./project/kaggle.json"
 KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
 export KAGGLE_CONFIG_DIR
 
-python3 ./project/trafficprediction.py.py
+python3 ./project/trafficprediction.py
