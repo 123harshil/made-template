a5e6328601d8bfada17a0321c58ccc34280b59ce
123harshil <53142888+123harshil@users.noreply.github.com>
2023-12-21 00:34:55 +0530

    Add files via upload

---
+++ project/pipeline.py
@@ -0,0 +1,33 @@
+import opendatasets as od
+import sqlite3
+import pandas as pd
+import logging
+
+# dataset links
+dataset_links = [
+  "https://www.kaggle.com/datasets/hasibullahaman/traffic-prediction-dataset", # elon must tweets
+  "https://www.kaggle.com/datasets/muthuj7/weather-dataset" # tesla stocks
+]
+
+# have to provide kaggle json file to download the .csv files
+od.download(dataset_links[0])
+
+
+# storing the elon must tweets into a sqlite database
+traffic_data = './traffic-prediction-dataset/Traffic.csv'
+df1 = pd.read_csv(traffic_data)
+traffic_db_path = '../made/traffic.sqlite'
+logging.info(f'storing the traffic data into a database under data folder ')
+conn = sqlite3.connect(traffic_db_path)
+df1.to_sql('traffic', conn, index=False, if_exists='replace')
+
+od.download(dataset_links[1])
+
+# storing the tesla tweets into a sqlite dataase
+weather_data = 'weather-dataset/weatherHistory.csv'
+df2 = pd.read_csv(weather_data)
+conn = sqlite3.connect("../made/weather.sqlite")
+logging.info(f'storing the weather data into a database under data folder ')
+df2.to_sql('weather', conn, index=False, if_exists='replace')
+# closing the database connection
+conn.close()
---
+++ project/tests.py
@@ -0,0 +1,72 @@
+import unittest
+import logging
+import os
+import sqlite3
+import pandas as pd
+
+
+class TestData(unittest.TestCase):
+
+    def setUp(self):
+        logging.info("setting up the data")
+        try:
+            # Set up SQLite databases
+            self.traffic_db_path = '../made/traffic.sqlite'
+            self.conn1 = sqlite3.connect(self.traffic_db_path)
+            self.query1 = f"SELECT * FROM traffic;"
+            self.df1 = pd.read_sql_query(self.query1, self.conn1)
+
+            self.weather_db_path = '../made/weather.sqlite'
+            self.conn2 = sqlite3.connect(self.weather_db_path)
+            self.query2 = f"SELECT * FROM weather;"
+            self.df2 = pd.read_sql_query(self.query2, self.conn2)
+        except Exception as e:
+            self.fail(f"Failed to set up: {e}")
+
+    def test_traffic_database(self):
+        logging.info("Running traffic database...")
+        try:
+            # Test if the traffic table exists in the database
+            cursor = self.conn1.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            tables = cursor.fetchall()
+            table_names = [table[0] for table in tables]
+            self.assertIn('traffic', table_names)
+            print("Test passed: traffic table exists in the database.")
+        except Exception as e:
+            self.fail(f"Test failed: {e}")
+
+    def test_weather_database(self):
+        logging.info("checking weather database...")
+        try:
+            # Test if the weather table exists in the database
+            cursor = self.conn2.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            tables = cursor.fetchall()
+            table_names = [table[0] for table in tables]
+            self.assertIn('weather', table_names)
+            print("Test passed: weather table exists in the database.")
+        except Exception as e:
+            self.fail(f"Test failed: {e}")
+
+    def test_df1(self):
+        logging.info("checking df1_dataframe...")
+        try:
+            # Test if the df1 DataFrame is not empty
+            self.assertFalse(self.df1.empty)
+            print("Test passed: DataFrame is not empty.")
+        except Exception as e:
+            self.fail(f"Test failed: {e}")
+
+    def test_df2(self):
+        logging.info("Running df2 dataframe...")
+        try:
+            # Test if the df2 DataFrame is not empty
+            self.assertFalse(self.df2.empty)
+            print("Test passed: DataFrame is not empty.")
+        except Exception as e:
+            self.fail(f"Test failed: {e}")
+
+
+if __name__ == '__main__':
+    unittest.main()
